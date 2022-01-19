dfb6217188dfa56fced1f77c3fe4dd826b2bffac
longnh176040 <thefollowingday.2099@gmail.com>
2022-01-19 11:19:50 +0700

    minor update set2

--- ObjectCounting/src/countingObjectSet2.py
+++ ObjectCounting/src/countingObjectSet2.py
@@ -6,7 +6,7 @@ fig = plt.figure(figsize=(10, 7))
 figR = 2
 figC = 3
 
-path = r'ObjectCounting/Project1Set2/object4.jpg'
+path = r'ObjectCounting/Project1Set2/object2.jpg'
 img = cv2.imread(path)
 
 if img is None:
@@ -23,11 +23,13 @@ try:
     fig.add_subplot(figR, figC, 2), plt.imshow(grayImage, cmap='gray')
     plt.title('Grey Scale Image'), plt.xticks([]), plt.yticks([])
 
+    # Tang do tuong phan
     gammaCorrect = np.array(255 * (grayImage / 255) ** 1.2 , dtype='uint8')
     fig.add_subplot(figR, figC, 3), plt.imshow(gammaCorrect, cmap='gray')
     plt.title('Gamma Correction Image'), plt.xticks([]), plt.yticks([])
 
     #=============== PROCESSING ================
+    # Phan vung su dung nguong thich nghi
     thresh = cv2.adaptiveThreshold(gammaCorrect, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 255, 19)
     thresh = cv2.bitwise_not(thresh)
     fig.add_subplot(figR, figC, 4), plt.imshow(thresh, cmap='gray')
@@ -38,32 +40,19 @@ try:
     img_dilation = cv2.dilate(thresh, kernel, iterations=1)
     img_erode = cv2.erode(img_dilation,kernel, iterations=1)
 
-    # clean all noise after dilatation and erosion
+    # Clean noise by median
     img_erode = cv2.medianBlur(img_erode, 7)
     fig.add_subplot(figR, figC, 5), plt.imshow(img_erode, cmap='gray')
     plt.title('Closing Image'), plt.xticks([]), plt.yticks([])
 
-# Labeling
+    #=============== COUNTING ================
     contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     rgb = img.copy()
     cv2.drawContours(rgb, contours, -1, (0, 255, 0), 1)
-
     fig.add_subplot(figR, figC, 6), plt.imshow(rgb)
     plt.title('Contour Image'), plt.xticks([]), plt.yticks([])
 
     print("Number of object", len(contours))
-
-    # ret, labels = cv2.connectedComponents(img_erode)
-    # label_hue = np.uint8(179 * labels / np.max(labels))
-    # blank_ch = 255 * np.ones_like(label_hue)
-    # labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
-    # labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
-    # labeled_img[label_hue == 0] = 0
-
-    # fig.add_subplot(figR, figC, 6), plt.imshow(labeled_img, cmap='gray')
-    # plt.title('Labeled Image'), plt.xticks([]), plt.yticks([])
-
-    # print('Number of objects is:', ret-1)
     plt.show()
 
 except Exception as e:
